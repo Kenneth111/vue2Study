8907570d7d75608eaa167733aa9a061aee8d7d3d
guokun <guokun@vanxum.com>
2017-08-18 18:03:13 +0800

    router可用了

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,61 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <el-button @click.native="startHacking">Let's do it</el-button>
+    <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">Processing Center</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">Workspace</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3"><a href="#" target="_blank">Orders</a></el-menu-item>
+    </el-menu>    
+    <el-row class="tac">
+      <el-col :span="3">
+        <el-menu theme="dark" default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>Navigator One</template>
+            <el-menu-item-group title="Group One">
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item one</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">item four</template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2"><i class="el-icon-menu"></i>Navigator Two</el-menu-item>
+          <el-menu-item index="3"><i class="el-icon-setting"></i>Navigator Three</el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="21">
+        <a href="/first">click me first</a>
+        <a href="/second">click me second</a>
+        <router-view class="view"></router-view>
+      </el-col>
+    </el-row>    
   </div>
 </template>
-
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Use Vue 2.0 Today!'
-    }
-  },
-
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It Works',
-        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
-        duration: 6000
-      })
-    }
+  import firstCom from './components/firstCom.vue'
+  export default {
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    components: {firstCom}
   }
-}
 </script>
-
 <style>
 body {
   font-family: Helvetica, sans-serif;
--- src/main.js
+++ src/main.js
@@ -2,10 +2,27 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import firstCom from './components/firstCom.vue'
+import secondcomponent from './components/secondCom.vue'
 Vue.use(ElementUI)
-
+Vue.use(VueRouter)
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/first',
+      component: firstCom
+    },
+    {
+      path: '/second',
+      component: secondcomponent
+    }
+  ]
+})
 new Vue({
+  router: router,
   el: '#app',
   render: h => h(App)
 })
