6a7a4cce6f87adb4bc199cc95fa6d2279e33c179
guokun <guokun@vanxum.com>
2017-08-19 11:26:20 +0800

    加入了vue-resource

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import firstCom from './components/firstCom.vue'
 import secondcomponent from './components/secondCom.vue'
 Vue.use(ElementUI)
 Vue.use(VueRouter)
+Vue.use(VueResource);
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
